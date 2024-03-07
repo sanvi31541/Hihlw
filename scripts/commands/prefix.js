e40392ede38e7c196289cee417367f315ae73d9d
badolbot35 <152863442+badolbot35@users.noreply.github.com>
2024-03-07 22:45:48 +0600

    Update prefix.js

--- scripts/commands/prefix.js
+++ scripts/commands/prefix.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "prefix",
   version: "1.0.0",
   permission: 0,
-  credits: "ryuko",
+  credits: "BADOL-KHAN",
   prefix: true,
   description: "guide",
   category: "system",
@@ -10,29 +10,53 @@ module.exports.config = {
   cooldowns: 5,
 };
 
-module.exports.handleEvent = async ({ event, api, Threads }) => {
-  var { threadID, messageID, body, senderID } = event;
-  function out(data) {
-    api.sendMessage(data, threadID, messageID)
+module.exports.handleEvent = async function ({ api, event }) {
+  const axios = require("axios");
+  const request = require("request");
+  const fs = require("fs");
+  const moment = require("moment-timezone");
+  var times = moment.tz("Asia/Dhaka").format("hh:mm:ss || D/MM/YYYY");
+  var thu = moment.tz("Asia/Dhaka").format("dddd");
+  moment.tz("Asia/Dhaka").format("dddd");
+  if (thu == "Sunday") thu = "রবিবার";
+  if (thu == "Monday") thu = "সোমবার";
+  if (thu == "Tuesday") thu = "মঙ্গলবার";
+  if (thu == "Wednesday") thu = "বুধবার";
+  if (thu == "Thursday") thu = "বৃহস্পতিবার";
+  if (thu == "Friday") thu = "শুক্রবার";
+  if (thu == "Saturday") thu = "শনিবার";
+  var { threadID, messageID, body } = event,
+    { PREFIX } = global.config;
+  let threadSetting = global.data.threadData.get(threadID) || {};
+  let prefix = threadSetting.PREFIX || PREFIX;
+const { createReadStream, existsSync, mkdirSync, readdirSync } = global.nodemodule["fs-extra"];
+			let { threadName, participantIDs } = await api.getThreadInfo(threadID);
+  const timeStart = Date.now();
+  const tdung = require("./../../B4D9L/Prefix.json");
+  var video = tdung[Math.floor(Math.random() * tdung.length)].trim();
+  function vtuanhihi(videoUrl, vtuandz, callback) {
+    request(videoUrl)
+      .pipe(fs.createWriteStream(__dirname + `/` + vtuandz))
+      .on("close", callback);
   }
-  var dataThread = (await Threads.getData(threadID));
-  var data = dataThread.data; 
-  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
-
-  var arr = ["mpre","mprefix","prefix", "command mark", "What is the prefix of the bot?","PREFIX"];
-  arr.forEach(i => {
-    let str = i[0].toUpperCase() + i.slice(1);
-    if (body === i.toUpperCase() | body === i | str === body) {
-		const prefix = threadSetting.PREFIX || global.config.PREFIX;
-      if (config.PREFIX == null) {
-        return out(`⚠️𝐌𝐲-𝐏𝐫𝐞𝐟𝐢𝐱📌👉( ${global.config.PREFIX} )`)
-      }
-      else return out(`⚠️𝐌𝐲-𝐏𝐫𝐞𝐟𝐢𝐱📌👉( ${global.config.PREFIX} )`)
-    }
-
-  });
+  if (body.toLowerCase() == "Prefix" || body.toLowerCase() == "prefix") {
+    let callback = function () {
+      return api.sendMessage(
+        {
+          body: `╭•┄┅══𝙈𝘾𝙎-𝘽𝘼𝘿𝙊𝙇-𝘽𝙊𝙏-𝟬𝟬𝟳══┅┄•╮\n\n==== ╰┈► 𝗠𝘆-𝗣𝗿𝗲𝗳𝗶𝘅-𝗖𝗺𝗱 ◄┈╯ ====\n\n•━━━━━━━━━━━━━━━━━━━━━━━━━━━━━•\n\n╰┈►𝗚𝗿𝗼𝘂𝗽-𝗡𝗮𝗺𝗲: ${threadName}\n\n╰┈►𝗠𝘆-𝗕𝗼𝘁-𝗣𝗿𝗲𝗳𝗶𝘅 : [ ${global.config.PREFIX} ]◄┈╯\n\n╰┈►𝗕𝗼𝘁-𝗡𝗮𝗺𝗲: ${global.config.BOTNAME}\n\n╰┈►𝗕𝗼𝘁-𝗔𝗹𝗹-𝗖𝗺𝗱-𝗟𝗶𝘀𝘁 :${
+            client.commands.size
+          } \n\n╰┈►𝗢𝘄𝗻𝗲𝗿-𝗟𝗶𝗻𝗸: m.me/100004504180813\n\n╰┈►𝗙𝗯- https://www.facebook.com/RAJA.BABU.TERA.REAL.ABBU.OK.07\n\n•━━━━━━━━━━━━━━━━━━━━━━━━━━━━━•\n\n『  ${thu} || ${times} 』\n\n•━━━━━━━━━━━━━━━━━━━━━━━━━━━━━•\nꗥ̳̳̳̳̳̳̳̳̳̳̿̿̿̿̿̿̿̿̿̿⃟ꗥꔸ𝐑𝐚𝐣𝐚-𝐁𝐚𝐛𝐮___//👿🤬😈ꔸꗥ⃟ꗥ̳̳̳̳̳̳̳̳̳̳̿̿̿̿̿̿̿̿̿̿\n\n╰•┄┅══𝙈𝘾𝙎-𝘽𝘼𝘿𝙊𝙇-𝘽𝙊𝙏-𝟬𝟬𝟳══┅┄•╯`,
+          attachment: [fs.createReadStream(__dirname + `/photo.jpeg`)],
+        },
+        event.threadID,
+        () => {
+          fs.unlinkSync(__dirname + `/photo.jpeg`);
+        },
+        event.messageID
+      );
+    };
+    vtuanhihi(video, "photo.jpeg", callback);
+  }
 };
 
-module.exports.run = async({ event, api }) => {
-    return api.sendMessage("no prefix commands", event.threadID)
-}
+module.exports.run = async ({ api, event, args, Threads }) => {};
