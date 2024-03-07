f442dbf36ad077e5fc51b3713d71d5c05dee974d
badolbot35 <152863442+badolbot35@users.noreply.github.com>
2024-03-08 00:14:52 +0600

    Update x.js

--- scripts/commands/x.js
+++ scripts/commands/x.js
@@ -18,10 +18,10 @@ module.exports.run = async function({ api, event, args }) {
 		const { createReadStream, unlinkSync } = global.nodemodule["fs-extra"];
 		const { resolve } = global.nodemodule["path"];
 		var content = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
-		var languageToSay = (["ru","en","ko","ja", "tl"].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
+		var languageToSay = (["bn",].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
 		var msg = (languageToSay != global.config.language) ? content.slice(3, content.length) : content;
-		const path = resolve(__dirname, 'cache', `${event.threadID}_${event.senderID}.mp3`);
-		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=${languageToSay}&client=tw-ob`, path);
-		return api.sendMessage({ attachment: createReadStream(path)}, event.threadID, () => unlinkSync(path), event.messageID);
+		const path = resolve(__dirname, 'cache', `☘️𝐁 𝐀 𝐃 𝐎 𝐋^𝐁 𝐎 𝐓^0 0 7🌺${event.threadID}_${event.senderID}.mp3`);
+		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=bn&client=tw-ob`, path);
+		return api.sendMessage({ attachment: createReadStream(path)}, event.threadID, () => unlinkSync(path));
 	} catch (e) { return console.log(e) };
-}
+			}
