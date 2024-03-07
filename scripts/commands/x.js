498823990ee7fc3eeb1423acf2d97825c1b3ae81
badolbot35 <152863442+badolbot35@users.noreply.github.com>
2024-03-07 19:16:42 +0600

    Update x.js

--- scripts/commands/x.js
+++ scripts/commands/x.js
@@ -1,28 +1,28 @@
 module.exports.config = {
 	name: "x",
-	version: "1.0.2",
+	version: "1.0.0",
 	permission: 0,
-	credits: "BADOL-KHAN",
-	prefix: true,
-	description: "check the amount of yourself or the person tagged",
-	category: "without prefix",
-	usages: "[tag]",
-	cooldowns: 5
+	credits: "ryuko",
+	description: "text to voice speech messages",
+  prefix: true,
+	category: "with prefix",
+	usages: `text to speech messages`,
+	cooldowns: 5,
+	dependencies: {
+		"path": "",
+		"fs-extra": ""
+	}
 };
-  
-  
-  
-  
-  
-  module.exports.run = async function({ api, event, args }) {
+
+module.exports.run = async function({ api, event, args }) {
 	try {
 		const { createReadStream, unlinkSync } = global.nodemodule["fs-extra"];
 		const { resolve } = global.nodemodule["path"];
 		var content = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
-		var languageToSay = (["bn",].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
+		var languageToSay = (["ru","en","ko","ja", "tl"].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
 		var msg = (languageToSay != global.config.language) ? content.slice(3, content.length) : content;
 		const path = resolve(__dirname, 'cache', `${event.threadID}_${event.senderID}.mp3`);
-		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=bn&client=tw-ob`, path);
-		return api.sendMessage({ attachment: createReadStream(path)}, event.threadID, () => unlinkSync(path));
+		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=${languageToSay}&client=tw-ob`, path);
+		return api.sendMessage({ attachment: createReadStream(path)}, event.threadID, () => unlinkSync(path), event.messageID);
 	} catch (e) { return console.log(e) };
 }
