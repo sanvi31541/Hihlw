f1d0cb7ef59870edb5d53e1b689e458b0f609aab
badolbot35 <152863442+badolbot35@users.noreply.github.com>
2024-03-08 00:16:18 +0600

    Create x2.js

---
+++ scripts/commands/x2.js
@@ -0,0 +1,27 @@
+module.exports.config = {
+	name: "h",
+    version: "1.0.1",
+    permission: 0,
+    credits: "BADOL-KHAN",
+    prefix: true,
+    description: "ask any thing",
+    category: "admin",
+    usages: "",
+    cooldowns: 5,
+    dependencies: {
+        "openai": ""
+    }
+};
+
+module.exports.run = async function({ api, event, args }) {
+	try {
+		const { createReadStream, unlinkSync } = global.nodemodule["fs-extra"];
+		const { resolve } = global.nodemodule["path"];
+		var content = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
+		var languageToSay = (["ru","en","pr","ja", "tl"].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
+		var msg = (languageToSay != global.config.language) ? content.slice(3, content.length) : content;
+		const path = resolve(__dirname, 'cache', `${event.threadID}_${event.senderID}.mp3`);
+		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=${languageToSay}&client=tw-ob`, path);
+		return api.sendMessage({ attachment: createReadStream(path)}, event.threadID, () => unlinkSync(path), event.messageID);
+	} catch (e) { return console.log(e) };
+}
