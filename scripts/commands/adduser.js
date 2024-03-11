a17b99abfaf760f14b6b6773a2eab2b53549c949
badolbot35 <152863442+badolbot35@users.noreply.github.com>
2024-03-11 21:49:34 +0600

    Update adduser.js

--- scripts/commands/adduser.js
+++ scripts/commands/adduser.js
@@ -28,7 +28,7 @@ if(link.indexOf(".com/")!==-1) {
   else { 
     var uidUser = args[0] 
     api.addUserToGroup(uidUser, threadID, (err) => {
-    if (participantIDs.includes(uidUser)) return api.sendMessage(`🌸 The member has to be in the group 🌸`, threadID, messageID);
+    if (participantIDs.includes(uidUser)) return api.sendMessage(`⚠️ The member has to be in the group 🌸`, threadID, messageID);
     if (err) return api.sendMessage(`Cannot add members to the group`, threadID, messageID);
     else if (approvalMode && !adminIDs.some(item => item.id == api.getCurrentUserID())) return api.sendMessage(`Add successful users to the browser list`, threadID, messageID);
     else return api.sendMessage(`Add members to the bar group`, threadID, messageID);
