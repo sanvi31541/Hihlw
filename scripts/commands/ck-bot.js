99f4e868fd8b03e97b4c713ae9fa53e72496bc12
badolbot35 <152863442+badolbot35@users.noreply.github.com>
2024-03-13 19:10:03 +0600

    Create ck-bot.js

---
+++ scripts/commands/ck-bot.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const jimp = require("jimp");
+const fs = require("fs");
+module.exports.config = {
+  name: "ckbot",
+  version: "1.0.2",
+  permission: 0,
+  credits: "BADOL-KHAN",
+  description: "beginner's guide",
+  prefix: true,
+  category: "guide",
+  usages: "[Shows Commands]",
+  cooldowns: 5,
+  envConfig: {
+    autoUnsend: true,
+    delayUnsend: 60
+  }
+};
+
+module.exports.run = async({api,event,args}) => {
+    const fs = global.nodemodule["fs-extra"];
+    const request = global.nodemodule["request"];
+    const threadSetting = global.data.threadData.get(parseInt(event.threadID)) || {};
+    const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
+     if (args.length == 0) return api.sendMessage(`You can use:\n\n${prefix}${this.config.name} user => it will take your own information.\n\n${prefix}${this.config.name} user @[Tag] => it will get friend information tag.\n\n${prefix}${this.config.name} box => it will get your box information (number of members, djt each other,...)\n\n${prefix}${this.config.name} user box [uid || tid.:\n\n${prefix}${this.config.name} admin => Admin Bot's Personal Information]`, event.threadID, event.messageID);
+    if (args[0] == "box") {
+           if(args[1]){ let threadInfo = await api.getThreadInfo(args[1]);
+           let imgg = threadInfo.imageSrc;
+           var gendernam = [];
+            var gendernu = [];
+                for (let z in threadInfo.userInfo) {
+                var gioitinhone = threadInfo.userInfo[z].gender;
+                if(gioitinhone == "MALE"){gendernam.push(gioitinhone)
+                }else{gendernu.push(gioitinhone)
+                }};
+             var nam = gendernam.length;
+             var nu = gendernu.length;
+             let sex = threadInfo.approvalMode;
+       var pd = sex == false ? "Turn off" : sex == true ? "turn on" : "NS";
+       if(!imgg) api.sendMessage(`Group name: ${threadInfo.threadName}\nTID: ${args[1]}\nApproved: ${pd}\nEmoji: ${threadInfo.emoji}\nInformation: \n»${threadInfo.participantIDs.length} members and ${threadInfo.adminIDs.length} administrators.\n»Including ${nam} boy and ${nu} female.\n»Total number of messages: ${threadInfo.messageCount}.`,event.threadID,event.messageID);
+        else var callback = () => api.sendMessage({body:`Group name: ${threadInfo.threadName}\nTID: ${args[1]}\nApproved: ${pd}\nEmoji: ${threadInfo.emoji}\nInformation: \n»${threadInfo.participantIDs.length} members and ${threadInfo.adminIDs.length}administrators.\n»Including ${nam} boy and ${nu} female.\n»Total number of messages: ${threadInfo.messageCount}.`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"), event.messageID); 
+      return request(encodeURI(`${threadInfo.imageSrc}`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
+      
+      }
+          
+            let threadInfo = await api.getThreadInfo(event.threadID);
+            let img = threadInfo.imageSrc;
