ae52027490e8c3f83ce32c64c8e1f344607b060d
badolbot35 <152863442+badolbot35@users.noreply.github.com>
2024-03-15 10:40:04 +0600

    Update logs.js

--- scripts/events/logs.js
+++ scripts/events/logs.js
@@ -1,8 +1,7 @@
-module.exports.config = {
 	name: "logs",
 	eventType: ["log:unsubscribe","log:subscribe","log:thread-name"],
 	version: "1.0.0",
-	credits: "Nayan",
+	credits: "BADOL-KHAN",
 	description: "record bot activity notifications",
     envConfig: {
         enable: true
@@ -10,28 +9,28 @@ module.exports.config = {
 };
 
 module.exports.run = async function({ api, event, Threads }) {
-    const logger = require("../../Nayan/catalogs/Nayanc.js");
+    const logger = require("../../utils/log");
     if (!global.configModule[this.config.name].enable) return;
-    var formReport =  "bot notification" +
-                        "\n\nthread id : " + event.threadID +
-                        "\naction : {task}" +
-                        "\nuser id : " + event.author +
-                        "\ndate : " + Date.now() +" ",
+    var formReport =  "=== 🌺বট আপডেট🌺 ===" +
+                        "\n\n» গ্রুপের আইডি কোড🌺: " + event.threadID +
+                        "\n» Action: {task}" +
+                        "\n»তার আইডি কোড🌺: " + event.author +
+                        "\n» " + Date.now() +" «",
         task = "";
     switch (event.logMessageType) {
         case "log:thread-name": {
-            const oldName = (await Threads.getData(event.threadID)).name || "name does not exist",
-                    newName = event.logMessageData.name || "name does not exist";
-            task = "user changes group name from : '" + oldName + "' to '" + newName + "'";
+            const oldName = (await Threads.getData(event.threadID)).name || "এবং নাম দিয়েছেন🌺",
+                    newName = event.logMessageData.name || "এবং নাম দিয়েছেন🌺";
+            task = "একজন গ্রুপের নাম চেঞ্জ করেছেন🌺: '" + oldName + "' to '" + newName + "'";
             await Threads.setData(event.threadID, {name: newName});
             break;
         }
         case "log:subscribe": {
-            if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) task = "the user added the bot to a new group";
+            if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) task = "একজন আপনার বট একটি নতুন গ্রুপের এড করেছে🌺";
             break;
         }
         case "log:unsubscribe": {
-            if (event.logMessageData.leftParticipantFbId== api.getCurrentUserID()) task = "the user kicked the bot out of the group"
+            if (event.logMessageData.leftParticipantFbId== api.getCurrentUserID()) task = "আপনার বটকে একটি গ্রুপ থেকে কিক দিয়া হয়েছে🌺"
             break;
         }
         default: 
@@ -42,10 +41,9 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
+  var god = [];
 
     return api.sendMessage(formReport, global.config.ADMINBOT[0], (error, info) => {
-        if (error) return logger("", "");
+        if (error) return logger(formReport, "[ Logging Event ]");
     });
-  return api.sendMessage(formReport, global.config.ADMINBOT[0]);
-}
-
+			     }
