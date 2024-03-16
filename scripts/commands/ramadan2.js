5db920b12c59af21de3f651a5b3a680f9deb3fc2
badolbot35 <152863442+badolbot35@users.noreply.github.com>
2024-03-16 20:28:38 +0600

    Update ramadan2.js

--- scripts/commands/ramadan2.js
+++ scripts/commands/ramadan2.js
@@ -3,7 +3,7 @@ module.exports.config = {
   version: "0.0.1",
   permission: 0,
   prefix: true,
-  credits: "Nayan",
+  credits: "BADOL-KHAN",//modified by Raja-Babu 
   description: "Ramadan",
   category: "user",
   usages: "",
@@ -16,7 +16,19 @@ module.exports.run = async function({
   api: a,
   args: n
 }) {
-  if (!n[0]) return a.sendMessage("====「 🆁🅰🅼🅰🅳🅰🅽 」====\n━━━━━━━━━━━━━\n𝟙.𝐒𝐞𝐡𝐫𝐢 𝐓𝐢𝐦𝐞\n𝟚.𝐈𝐟𝐭𝐚𝐫 𝐓𝐢𝐦𝐞\n𝟛. 𝐈𝐟𝐭𝐚𝐫 𝐃𝐮𝐚\n𝟜. 𝐑𝐮𝐣𝐚𝐫 𝐍𝐢𝐲𝐨𝐭\n\n𝐑𝐞𝐩𝐥𝐲 𝐓𝐡𝐢𝐬 𝐌𝐬𝐠 𝐀𝐧𝐝 𝐒𝐞𝐥𝐞𝐜𝐭 𝐍𝐮𝐦𝐛𝐞𝐫", e.threadID, ((a, n) => {
+const axios = require('axios')
+const links = [
+    "https://i.imgur.com/dYJQDBQ.jpeg",
+    "https://i.imgur.com/Y8WaJUt.jpeg",
+    "https://i.imgur.com/dYJQDBQ.jpeg",
+    "https://i.imgur.com/Y8WaJUt.jpeg",
+    "https://i.imgur.com/dYJQDBQ.jpeg",
+  ];
+  const rahad = links[Math.floor(Math.random() * links.length)];
+  const rahad2 = (await axios.get(rahad, { responseType: 'stream' })).data;
+  
+const rahad3 = "====╰┈► 🅡🅐🅜🅐🅓🅐🅝 ◄┈╯====\n★★▰▱▰▱▰▱▰▰▱▰▱▰▱▰▱▰▱▰★★\n𝟙. 𝐒𝐞𝐡𝐫𝐢 𝐓𝐢𝐦𝐞◄┈╯\n𝟚. 𝐈𝐟𝐭𝐚𝐫 𝐓𝐢𝐦𝐞◄┈╯\n𝟛. 𝐈𝐟𝐭𝐚𝐫 𝐃𝐮𝐚◄┈╯\n𝟜. 𝐑𝐮𝐣𝐚𝐫 𝐍𝐢𝐲𝐨𝐭◄┈╯\n𝟝. 𝐑𝐮𝐣𝐚 𝐕𝐚𝐧𝐠𝐚𝐫 𝐊𝐚𝐫𝐨𝐧◄┈╯\n𝟞. 𝐉𝐞𝐠𝐮𝐥𝐚 𝐊𝐨𝐫𝐥𝐞 𝐑𝐮𝐣𝐚 𝐕𝐚𝐧𝐠𝐞 𝐧𝐚◄┈╯\n\n𝐑𝐞𝐩𝐥𝐲 𝐓𝐡𝐢𝐬 𝐌𝐬𝐠 𝐀𝐧𝐝 𝐒𝐞𝐥𝐞𝐜𝐭 𝐍𝐮𝐦𝐛𝐞𝐫\n\n★★▰▱▰▱▰▱▰▰▱▰▱▰▱▰▱▰▱▰★★\n\n☑️${juswa}☑️"
+  if (!n[0]) return a.sendMessage({body: rahad3, attachment: rahad2}, e.threadID, ((a, n) => {
     global.client.handleReply.push({
       name: this.config.name,
       messageID: n.messageID,
@@ -39,11 +51,11 @@ module.exports.run = async function({
     const n = (await p.get(h)).data.data;
     const time = (await p.get(h)).data.times.time;
     const msg = (await p.get(h)).data.msg;
-    
-    
+
+
     return e.sendMessage({
       body: `${msg}: ${time}`
-      
+
     }, a.threadID, a.messageID)
   }
 
@@ -58,6 +70,10 @@ module.exports.run = async function({
          var   h = `${n}/nayan/iftardua`;
       else if ("4" == a.body)
          var   h = `${n}/nayan/rujarniyot`;
+      else if ("5" == a.body)
+         var   h = `${n}/nayan/rujavk`;
+      else if ("6" == a.body)
+         var   h = `${n}/nayan/rujavkn`;
         return { p, h };
     }
 };
