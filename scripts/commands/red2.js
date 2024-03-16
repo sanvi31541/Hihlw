20199162feb47578b20374b25553ba964219fae3
badolbot35 <152863442+badolbot35@users.noreply.github.com>
2024-03-16 19:40:38 +0600

    Update red2.js

--- scripts/commands/red2.js
+++ scripts/commands/red2.js
@@ -4,7 +4,7 @@ module.exports.config = {
   version: "1.0.0", 
   permission: 2,
   credits: "BADOL-KHAN",
-  description: "Random sad video",
+  description: "Random red2 video",
   prefix: true,
   category: "Media", 
   usages: "video", 
@@ -20,13 +20,13 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["🤙 ভিডিও দেখে কেউ হাত মারলে আমি দায়ি নয়😑"];
+   var hi = ["🤧 ভিডিও দেখে কেউ হাত মারলে আমার দোস নাই🤙"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
   "https://drive.google.com/uc?id=1fm7FndVaN4Xj3YdtJoZcT5Bbov_aqT1E",
 
-    "https://drive.google.com/uc?id=1f33Lc7bwXxwz3ol0ndqMbQ96xab70G7L",
+"https://drive.google.com/uc?id=1f33Lc7bwXxwz3ol0ndqMbQ96xab70G7L",
     "https://drive.google.com/uc?id=1fsQRBliBOloH_DkGAetJiVH4bfRYWbuR",
     "https://drive.google.com/uc?id=1fQn9GmU-PlToslACaHJSDWzUJVFZ6fBy",
     "https://drive.google.com/uc?id=1fck0SulaPt4EQU-hr0eKWMkCEW14kWH0",
@@ -34,7 +34,7 @@ const fs = global.nodemodule["fs-extra"];
     "https://drive.google.com/uc?id=1fm4eNlkcug-urU8vSjpxb0WZzC6WJH46",
     "https://drive.google.com/uc?id=1fGzuPxwqHteoPtD9TfIla02GX0PqVLr1",
     "https://drive.google.com/uc?id=1fFYfemXPS4ni9Q6C6cYukWDZy-PUKZCq",
-    "https://drive.google.com/uc?id=1fWxwp1IOg8xGkqkFygOPKQBX99Nivx7d",
+    "https://drive.google.com/uc?id=1fs0wek6OF9hfpfryHZ3n0KBE-ZSfFacc",
 ];
      var callback = () => api.sendMessage({body:`「 ${know} 」`,attachment: fs.createReadStream(__dirname + "/cache/15.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/15.mp4"));    
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/15.mp4")).on("close",() => callback());
