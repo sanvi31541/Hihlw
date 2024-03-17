923bfe663910329b00be8b44094636db6f3c22f2
badolbot35 <152863442+badolbot35@users.noreply.github.com>
2024-03-17 23:54:34 +0600

    Create gameni.js

---
+++ scripts/commands/gameni.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+
+module.exports.config = {
+  name: "gemini",
+  version: "2.0.0",
+  permission: 0,
+  credits: "BADOL-KHAN",
+  description: "gemini ai", // Fixed typo here
+  prefix: true,
+  category: "google",
+  usages: "{pn} message | photo reply",
+  cooldowns: 5,
+};
+
+module.exports.run = async function ({ api, args, event }) {
+  const prompt = args.join(' ');
+
+  //---- Image Reply -----//
+  if (event.type === "message_reply") {
+    var t = event.messageReply.attachments[0].url;
+    try {
+      const response = await axios.get(`https://all-image-genator-d1p.onrender.com/dipto/gemini?.prompt=${encodeURIComponent(prompt)}&url=${encodeURIComponent(t)}`)
+      const data2 = response.data.dipto;
+      api.sendMessage(data2, event.threadID, event.messageID);
+    } catch (error) {
+      console.error('Error:', error.message);
+      api.sendMessage(error, event.threadID, event.messageID);
+    }
+  }
+  //---------- Message Reply ---------//
+  else if (!prompt) {
+    return api.sendMessage('Please provide a prompt or message reply', event.threadID, event.messageID);
+  } else {
+    try {
+      const respons = await axios.get(`https://all-image-genator-d1p-onrender.com/dipto/gemini?,prompt=${encodeURIComponent(prompt)}`)
+      const message = respons.data.dipto;
+      api.sendMessage(message, event.threadID, event.messageID);
+    } catch (error) {
+      console.error('Error calling Gemini AI:', error);
+      api.sendMessage(`Sorry, there was an error processing your request.${error}`, event.threadID, event.messageID);
+    }
+  }
+};
